abec897bcf5b511f3d878f70e06bb3f527b47508
Tiaujah Williams <92950471+Tiaujahw@users.noreply.github.com>
2023-04-30 10:25:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const wrapper = document.querySelector('.wrapper');
+const registerLink = document.querySelector('.register-link');
+const loginLink = document.querySelector('.login-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+registerLink.onclick = () => {
+    wrapper.classList.add('active');
+};
+
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+};
+
+btnPopup.onclick = () => {
+    wrapper.classList.add('active-popup');
+};
+
+
+iconClose.onclick = () => {
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+};
+
+//code for webpage switch
+
+const bgAnimation = document.getElementById('bgAnimation');
+
+const numberOfColorBoxes = 400;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
+
